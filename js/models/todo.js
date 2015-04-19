8f1a1af48fb19b38f6be1009785040a8f7f34abd
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-04-19 08:01:40 -0500

    add priority button

--- js/models/todo.js
+++ js/models/todo.js
@@ -10,10 +10,11 @@ var app = app || {};
 	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
 	app.Todo = Backbone.Model.extend({
 		// Default attributes for the todo
-		// and ensure that each todo created has `title` and `completed` keys.
+		// and ensure that each todo created has `title`, `completed`, and `priority` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			priority: false
 		},
 
 		// Toggle the `completed` state of this todo item.
@@ -22,8 +23,16 @@ var app = app || {};
 				completed: !this.get('completed'),
 				title: this.attributes.title + '--Done'
 			});
+		},
 
-			// console.log(this.attributes.title + '--Done');
+		// Toggle the `priority` state of this todo item.
+		togglePriority: function () {
+			this.save({
+				priority: !this.get('priority')
+			});
+			// this.addClass('priority');
+			console.log(this);
 		}
+
 	});
 })();
